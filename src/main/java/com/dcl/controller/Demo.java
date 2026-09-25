8210cbfadf71ae2b7ce54fb616f39e6cb062b5a8
ArjunJR2004 <arjunjr.dev@gmail.com>
2026-09-25 16:11:12 +0530

    Added RestController

--- src/main/java/com/dcl/controller/Demo.java
+++ src/main/java/com/dcl/controller/Demo.java
@@ -2,8 +2,9 @@ package com.dcl.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class Demo {
 
 	@GetMapping("/path")
